b44f6f2511a4e36eb9be859411bb8747dcdb224b
khalidbouruki <kbouruki@cca.edu>
2017-05-26 14:00:33 -0700

    Update 526 Analog Input Modified.ino

--- 526 Analog Input Modified.ino
+++ 526 Analog Input Modified.ino
@@ -1,8 +1,8 @@
 
+
 int sensorPin = A0;    // select the input pin for the potentiometer
 int ledPin = 13;      // select the pin for the LED
 int sensorValue = 0;  // variable to store the value coming from the sensor
-const int threshold = 800;
 
 void setup() {
    // declare the ledPin as an OUTPUT:
@@ -14,14 +14,9 @@ void setup() {
 void loop() {
  // read the value from the sensor:
   int sensorValue = analogRead(sensorPin);  
-
-  // if the analog value is high enough, turn on the LED:
-  if (sensorValue > threshold) {
     digitalWrite(ledPin, HIGH);
-  } else {
     digitalWrite(ledPin, LOW);
-  }
-  
+ 
      // print out the value you read:
   Serial.println(sensorValue);
   // delay in between reads for stability
